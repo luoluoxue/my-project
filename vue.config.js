3be4ce9c163e53d76a316ddb5893243fed3972a1
luoluoxue <473887719@qq.com>
2023-11-15 17:21:46 +0800

    update:修改

--- vue.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  transpileDependencies: true,
-  publicPath: './', // 自己GitHub上的仓库名称两边 / 不能省略
-  outputDir: 'docs', // 打包输出的文件名称（不配置也可以，默认是dist）
-  assetsDir: 'static', // 静态资源（js、css等）存放路径，相对outputDir
-}
