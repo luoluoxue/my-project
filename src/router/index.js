3a10663a9695efecaad677700d654a90222dbac6
luoluoxue <473887719@qq.com>
2023-11-20 10:49:14 +0800

    update:修改router

--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,21 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-
+import Router from 'vue-router'
 import Home from '@/views/home'
 import My from '@/views/my'
 
-// const Home = () => import("@/views/home");
-// const My = () => import("@/views/my");
+Vue.use(Router)
 
-Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/',
-    redirect: 'home'
-  },
-  {
-    path: '/home',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/my',
-    name: 'my',
-    component: My
-  }
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  routes
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/my',
+      name: 'my',
+      component: My
+    }
+  ]
 })
-
-export default router
